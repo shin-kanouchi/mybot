4005229793278529ad3720e29acb90ed36790782
shin-kanouchi <tenzyonerai@gmail.com>
2016-10-07 21:46:42 +0900

    修正ボタンを作る前の所まで

--- app/controllers/trains_controller.rb
+++ app/controllers/trains_controller.rb
@@ -8,26 +8,36 @@ class TrainsController < ApplicationController
 
   def new
     @tweet = Sentence.where( 'id >= ?', rand(Sentence.count) + 1 ).first
-    client = Docomoru::Client.new(api_key: '6c61546d47537763524d6e577a68716b4e70586e49465542326a45432e6c762e41347359366d79756a492f')
-    @docomo_reply = client.create_dialogue(@tweet.sentence).body["utt"]
-    sent = Sentence.where(sentence: @docomo_reply).first_or_initialize #DBに含まれていたらそれをとる。無ければnew()
-    sent.save
-    @train = Train.new(tweet_id: @tweet.id)
+
+    #client = Docomoru::Client.new(api_key: '6c61546d47537763524d6e577a68716b4e70586e49465542326a45432e6c762e41347359366d79756a492f')
+    #@docomo_reply = client.create_dialogue(@tweet.sentence).body["utt"]
+    #sent = Sentence.where(sentence: @docomo_reply).first_or_initialize #DBに含まれていたらそれをとる。無ければnew()
+    #sent.save
+    #@train = Train.new(tweet_id: @tweet.id)
   end
 
-  def create    
-    if params[:train][:sentence_id] == ""
-      @new_reply = Sentence.create(sentence: params[:text_area])
-      Train.create(tweet_id: @tweet.id, reply_id: @new_reply.id, adequacy_flag: 1) #hogeのID
-    else
-      Train.create(create_params)
-    end
-    redirect_to controller: :trains, action: :index
+  def create
+    #訂正がされている場合
+    #if params[:train][:sentence_id] == "" 
+    #  @new_reply = Sentence.create(sentence: params[:text_area])
+    #  Train.create(tweet_id: @tweet.id, reply_id: @new_reply.id, adequacy_flag: 1) #hogeのID
+
+    #訂正されていない場合
+    #else
+    binding.pry
+    @tweet = Sentence.create(sent_params)
+    #end
+    redirect_to controller: :trains, action: :new
   end
 
   private
-  def create_params
+  def sent_params
+    params.require(:sentence).permit(:sentence)
+  end
+
+  def train_params
     params.require(:train).permit(:reply_id).merge(adequacy_flag: 1)
   end
+
 
 end
--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -3,4 +3,16 @@ class Sentence < ActiveRecord::Base
   has_many :replys, through: :tweet_replys
   has_many :reply_tweets, class_name: "Train", foreign_key: :reply_id
   has_many :tweets, through: :reply_tweets
+
+  def docomo_reply
+    client = Docomoru::Client.new(api_key: '6c61546d47537763524d6e577a68716b4e70586e49465542326a45432e6c762e41347359366d79756a492f')
+    while true
+      reply = client.create_dialogue(self.sentence).body["utt"]
+      if reply.length < 40
+        break
+      end
+    end
+    Sentence.where(sentence: reply).first_or_create #DBに含まれていたらそれをとる。無ければnew()
+  end
+
 end
