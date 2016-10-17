815dbb976a3448140f78726d043c1d54832b5b9a
shin-kanouchi <tenzyonerai@gmail.com>
2016-10-17 16:07:08 +0900

    edit calc_score of rank

--- app/controllers/pairwises_controller.rb
+++ app/controllers/pairwises_controller.rb
@@ -21,13 +21,54 @@ class PairwisesController < ApplicationController
     if @eval.pairwises.count < 10
       redirect_to controller: :pairwises, action: :new
     else
-      @eval.win_flag = @eval.pairwises.group(:inequality_flag).order('count_inequality_flag DESC').limit(1).count(:inequality_flag).keys[0]
+      #勝敗計算
+      @eval.win_flag = calc_win_flag
       @eval.save
+
+      #バトルポイント付与
       bot = Bot.where('user_id = ?', current_user.id).first
       bot.battle_point += 1
       bot.save
+
+      #rankの操作
+      calc_rank
+
       redirect_to controller: :pairwises, action: :index
     end
   end
+
+  def calc_win_flag
+    scores = @eval.pairwises.group(:inequality_flag).order('count_inequality_flag DESC').limit(3).count(:inequality_flag)
+    x_score = 0
+    y_score = 0
+    x_score += scores[1] if scores.has_key?(1)
+    y_score += scores[2] if scores.has_key?(2)
+
+    if x_score > y_score
+      return 1
+    elsif x_score < y_score
+      return 2
+    else
+      return 0
+    end
+  end
+
+  def calc_rank
+    bot_x = Bot.where(user_id: @eval.user_x_id).first
+    bot_y = Bot.where(user_id: @eval.user_y_id).first
+    if @eval.win_flag == 1
+      bot_x.bot_rank += 10
+      bot_y.bot_rank -= 5
+    elsif @eval.win_flag == 2
+      bot_x.bot_rank -= 5
+      bot_y.bot_rank += 10
+    else
+      bot_x.bot_rank += 2
+      bot_y.bot_rank += 2
+    end
+    bot_x.save
+    bot_y.save
+  end
+
 
 end
